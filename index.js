9c2af5496ea29bfce266c7b032e8ce9d983e58cb
Web Developer <134505974+expertwebdeveloperqueen@users.noreply.github.com>
2023-08-08 22:32:55 +0500

    Animated-button

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+let btn = document.querySelector('a');
+btn.addEventListener('mousemove', e =>{
+    let rect=e.target.getBoundingClientRect();
+    let x=e.clientX*5-rect.left;
+    btn.style.setProperty('--x', x + 'deg');
+})
